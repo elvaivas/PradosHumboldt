b8363f8c437aa06967dc3e4f5ddf621f5385c4f7
Jesus Gabriel Vivas Gomez <elvaivas@gmail.com>
2021-01-20 22:03:41 -0400

    Primera propuesta de proyecto de pagina para el condominio

---
+++ datos.js
@@ -0,0 +1,31 @@
+var data={
+    "titulo":"Recidencias Prado Humboldt",
+    "cuji":[
+        {"nombre":"Jesus Gabriel", "apellido":"Vivas Gomez", "cedula":"20740160", "deuda":11254151},
+        {"nombre":"Jesus Gabriel", "apellido":"Vivas Gomez", "cedula":"20740160", "deuda":11254151},
+        {"nombre":"Jesus Gabriel", "apellido":"Vivas Gomez", "cedula":"20740160", "deuda":11254151},
+        {"nombre":"Jesus Gabriel", "apellido":"Vivas Gomez", "cedula":"20740160", "deuda":11254151},
+        {"nombre":"Jesus Gabriel", "apellido":"Vivas Gomez", "cedula":"20740160", "deuda":11254151}
+    ],
+    "datilera":[
+        {"nombre":"Juvencio Vivas", "apellido":"Vivas Gomez", "cedula":"20740160", "deuda":11254151},
+        {"nombre":"Juvencio Vivas", "apellido":"Vivas Gomez", "cedula":"20740160", "deuda":11254151},
+        {"nombre":"Juvencio Vivas", "apellido":"Vivas Gomez", "cedula":"20740160", "deuda":11254151},
+        {"nombre":"Juvencio Vivas", "apellido":"Vivas Gomez", "cedula":"20740160", "deuda":11254151},
+        {"nombre":"Juvencio Vivas", "apellido":"Vivas Gomez", "cedula":"20740160", "deuda":11254151}
+    ],
+    "acacias":[
+        {"nombre":"Alejandro Jose", "apellido":"Vivas Gomez", "cedula":"20740160", "deuda":11254151},
+        {"nombre":"Alejandro Jose", "apellido":"Vivas Gomez", "cedula":"20740160", "deuda":11254151},
+        {"nombre":"Alejandro Jose", "apellido":"Vivas Gomez", "cedula":"20740160", "deuda":11254151},
+        {"nombre":"Alejandro Jose", "apellido":"Vivas Gomez", "cedula":"20740160", "deuda":11254151},
+        {"nombre":"Alejandro Jose", "apellido":"Vivas Gomez", "cedula":"20740160", "deuda":11254151}
+    ],
+    "bucare":[
+        {"nombre":"Juvelis Maria", "apellido":"Vivas Gomez", "cedula":"20740160", "deuda":11254151},
+        {"nombre":"Juvelis Maria", "apellido":"Vivas Gomez", "cedula":"20740160", "deuda":11254151},
+        {"nombre":"Juvelis Maria", "apellido":"Vivas Gomez", "cedula":"20740160", "deuda":11254151},
+        {"nombre":"Juvelis Maria", "apellido":"Vivas Gomez", "cedula":"20740160", "deuda":11254151},
+        {"nombre":"Juvelis Maria", "apellido":"Vivas Gomez", "cedula":"20740160", "deuda":11254151}
+    ]
+}
